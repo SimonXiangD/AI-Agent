586679efa91dbe82a2048084eaa5bbdb8437a5b7
Yang Xiang (FESCO Adecco Human Resources) <v-yangxiang@microsoft.com>
2024-04-28 12:43:11 +0800

    feat/add animatioin with script control

--- Assets/Scripts/MotionSystem/PlayMovement.cs
+++ Assets/Scripts/MotionSystem/PlayMovement.cs
@@ -7,10 +7,21 @@ public class PlayMovement : MonoBehaviour
     public float speed;
     public float rotationSpeed;
 
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
+        horizontalInput *= -1;
+        verticalInput *= -1;
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
@@ -19,8 +30,13 @@ public class PlayMovement : MonoBehaviour
         
         if (movementDirection != Vector3.zero)
         {
+            animator.SetBool("isMoving", true);
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
 }
